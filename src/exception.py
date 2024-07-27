64d0f01526cc7423dd4d42953781572bf4098412
TarunRamapuram <tarunramapuram@gmail.com>
2024-07-27 13:32:40 +0530

    Exception_Modification

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # provides all information about exceptions using sys and exc_info method.
