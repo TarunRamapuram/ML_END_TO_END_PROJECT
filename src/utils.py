7b3a991f2155549b44ec85013c555c5c8ca10c38
TarunRamapuram <tarunramapuram@gmail.com>
2024-07-27 20:21:32 +0530

    Data Transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,23 @@
 # Functionalities that are common to the project.
+
+import os
+import sys
+import dill 
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+# helps us to create pickel file
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
